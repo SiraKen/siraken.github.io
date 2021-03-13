4c7b078a91a95b123109e1b04fb5e180b19a4912
SiraKen <shirasawa.kento@gmail.com>
2021-03-13 10:55:57 +0000

    Deploying to gh-pages from  @ 803668b01be2f3c0fcd9decc7e614dc73b7304c8 🚀

---
+++ _nuxt/static/1615632956/2021-02-23_move-to-nuxt-content/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615632956",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{content:{dbHash:"8c64b2f0"}}}}("\u002F2021-02-23_move-to-nuxt-content"));
